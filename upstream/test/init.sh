3e255ea5d515db1fc3d7c9906c5b1c78b08e33fa
Jagerzhang <jagerzhang@tencent.com>
2021-11-26 09:40:40 +0800

    clear no used files

--- upstream/test/init.sh
+++
@@ -1,3 +0,0 @@
-curl -X PUT -d@upstream.json http://127.0.0.1:9080/apisix/admin/upstreams/test -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1'
-curl -X PUT -d@service.json http://127.0.0.1:9080/apisix/admin/services/test -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1'
-curl -X PUT -d@route.json http://127.0.0.1:9080/apisix/admin/routes/test -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1'
